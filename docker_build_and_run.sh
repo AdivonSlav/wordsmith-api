34f0685699ab95c4f46d0ebc069f4dc2f3fd033c
Adi Efendic <efendic100@gmail.com>
2023-09-03 20:55:29 +0200

    create helper scripts for docker build and docker compose

---
+++ docker_build_and_run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Define vars
+api_tag="wordsmith/wordsmith_api"
+api_dockerfile="Wordsmith.API/Dockerfile"
+
+identityserver_tag="wordsmith/wordsmith_identityserver"
+identityserver_dockerfile="Wordsmith.IdentityServer/Dockerfile"
+
+# Build image
+echo "Building image $api_tag..."
+docker build -f $api_dockerfile -t $api_tag:latest .
+
+# Check build status
+if [ $? -eq 0 ]; then
+  echo "$api_tag built succesfully"
+else
+  echo "$api_tag build failed"
+  exit 1
+fi
+
+# Build image
+echo "Building image $identityserver_tag..."
+docker build -f $identityserver_dockerfile -t $identityserver_tag:latest .
+
+# Check build status
+if [ $? -eq 0 ]; then
+  echo "$identityserver_tag built succesfully"
+else
+  echo "$identityserver_tag build failed"
+  exit 1
+fi
+
+# Run docker compose
+echo "All images built succesfully, proceeding to docker compose..."
+docker compose up -d

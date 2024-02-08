e3847f09205db39257a12cdfe48a5e63eecc506a
AdivonSlav <efendic100@gmail.com>
2024-02-08 13:06:44 +0100

    refactor OrderByProperty to allow nested property sorting

--- Wordsmith.DataAccess/src/Extensions/QueryExtensions.cs
+++ Wordsmith.DataAccess/src/Extensions/QueryExtensions.cs
@@ -8,16 +8,30 @@ public static class QueryExtensions
     public static IQueryable<T> OrderByProperty<T>(this IQueryable<T> source, string propertyName, bool ascending = true)
     {
         var entityType = typeof(T);
-        var property = entityType.GetProperty(propertyName);
-        
-        if (property == null) throw new AppException("Selected property for orderBy does not exist!");
-
+        var properties = propertyName.Split(".");
         var parameter = Expression.Parameter(entityType, "x");
-        var propertyAccess = Expression.Property(parameter, property);
+        Expression propertyAccess = parameter;
+        
+        foreach (var name in properties)
+        {
+            var property = entityType.GetProperty(name);
+            
+            if (property == null)
+                throw new ArgumentException($"Property '{name}' not found in type '{entityType.FullName}'");
+
+            propertyAccess = Expression.Property(propertyAccess, property);
+            entityType = property.PropertyType;
+        }
+        
         var lambda = Expression.Lambda(propertyAccess, parameter);
         var methodName = ascending ? "OrderBy" : "OrderByDescending";
-        var methodCallExpression = Expression.Call(typeof(Queryable), methodName,
-            new[] { entityType, property.PropertyType }, source.Expression, Expression.Quote(lambda));
+        var methodCallExpression = Expression.Call(
+            typeof(Queryable),
+            methodName,
+            new[] { typeof(T), propertyAccess.Type },
+            source.Expression,
+            Expression.Quote(lambda)
+        );
 
         return source.Provider.CreateQuery<T>(methodCallExpression);
     }

be589445f9fb926dea42af16ad23ffdca5009796
AdivonSlav <efendic100@gmail.com>
2024-03-18 20:37:42 +0100

    add seed data for books

---
+++ Wordsmith.Models/src/SeedObjects/BookSeed.cs
@@ -0,0 +1,14 @@
+namespace Wordsmith.Models.SeedObjects;
+
+public class BookSeed
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double RatingAverage { get; set; }
+    public int ChapterCount { get; set; }
+    public decimal? Price { get; set; }
+    public string Genres { get; set; }
+    public string MaturityRating { get; set; }
+    public string BookFilename { get; set; }
+    public string ImageFilename { get; set; }
+}

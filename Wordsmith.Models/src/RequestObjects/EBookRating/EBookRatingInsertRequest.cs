3b9acd17b8c0d3e28c73f49da7b0bab6fe560dd8
AdivonSlav <efendic100@gmail.com>
2024-03-19 14:21:00 +0100

    fix validation for rating insert and update

--- Wordsmith.Models/src/RequestObjects/EBookRating/EBookRatingInsertRequest.cs
+++ Wordsmith.Models/src/RequestObjects/EBookRating/EBookRatingInsertRequest.cs
@@ -4,9 +4,11 @@ namespace Wordsmith.Models.RequestObjects.EBookRating;
 
 public class EBookRatingInsertRequest
 {
+    [Required]
     [Range(minimum: 1, maximum: 5, ErrorMessage = "Ratings must be from 1 to 5")]
     public int Rating { get; set; }
     
+    [Required]
     public int EBookId { get; set; }
     
     public int UserId { get; set; }
--- Wordsmith.Models/src/RequestObjects/EBookRating/EBookRatingUpdateRequest.cs
+++ Wordsmith.Models/src/RequestObjects/EBookRating/EBookRatingUpdateRequest.cs
@@ -4,6 +4,7 @@ namespace Wordsmith.Models.RequestObjects.EBookRating;
 
 public class EBookRatingUpdateRequest
 {
+    [Required]
     [Range(minimum: 1, maximum: 5, ErrorMessage = "Ratings must be from 1 to 5")]
     public int Rating { get; set; }
 }

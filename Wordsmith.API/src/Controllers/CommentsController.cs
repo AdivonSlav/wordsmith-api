9555a6e2be191a4a1dcc6371626d738e9aefb6e4
AdivonSlav <efendic100@gmail.com>
2024-03-19 14:52:34 +0100

    add annotations and authorization to comments endpoints

--- Wordsmith.API/src/Controllers/CommentsController.cs
+++ Wordsmith.API/src/Controllers/CommentsController.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using Wordsmith.DataAccess.Db.Entities;
 using Wordsmith.DataAccess.Services.Comment;
 using Wordsmith.Models.DataTransferObjects;
@@ -12,4 +14,19 @@ namespace Wordsmith.API.Controllers;
 public class CommentsController : WriteController<CommentDto, Comment, CommentSearchObject, CommentInsertRequest, CommentUpdateRequest>
 {
     public CommentsController(ICommentService commentService) : base(commentService) { }
+
+    [SwaggerOperation("Insert a new comment for an ebook or chapter")]
+    [Authorize("All")]
+    public override Task<ActionResult<EntityResult<CommentDto>>> Insert(CommentInsertRequest insert)
+    {
+        insert.UserId = GetAuthUserId();
+        return base.Insert(insert);
+    }
+
+    [SwaggerOperation("Update a comment")]
+    [Authorize("Admin")]
+    public override Task<ActionResult<EntityResult<CommentDto>>> Update(int id, CommentUpdateRequest update)
+    {
+        return base.Update(id, update);
+    }
 }

7a09e58e908de7acc6bef61c503371121efb7968
AdivonSlav <efendic100@gmail.com>
2024-03-28 12:41:19 +0100

    fix pagination default values

--- Wordsmith.Models/src/SearchObjects/SearchObject.cs
+++ Wordsmith.Models/src/SearchObjects/SearchObject.cs
@@ -7,11 +7,11 @@ public class SearchObject
 {
     [SwaggerParameter("The requested page")]
     [DefaultValue(1)]
-    public int Page { get; } = 1;
+    public int Page { get; init;  }
 
     [SwaggerParameter("The item count that a page should have")]
     [DefaultValue(100)]
-    public int PageSize { get; } = 100;
+    public int PageSize { get; init;  }
     
     [SwaggerParameter("Whether the result should be ordered by a specific property, e.g. PropertyA:asc")]
     public string? OrderBy { get; set; }

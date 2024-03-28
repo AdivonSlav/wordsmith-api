83f0a6ab27b96a42c3b6e5f61f18a5de7e87f9f8
AdivonSlav <efendic100@gmail.com>
2024-03-28 16:27:37 +0100

    fix search object default values

--- Wordsmith.Models/src/SearchObjects/SearchObject.cs
+++ Wordsmith.Models/src/SearchObjects/SearchObject.cs
@@ -7,15 +7,12 @@ public class SearchObject
 {
     [SwaggerParameter("The requested page")]
     [DefaultValue(1)]
-    public int Page { get; init;  }
+    public int Page { get; init; } = 1;
 
     [SwaggerParameter("The item count that a page should have")]
     [DefaultValue(100)]
-    public int PageSize { get; init;  }
+    public int PageSize { get; init; } = 100;
     
     [SwaggerParameter("Whether the result should be ordered by a specific property, e.g. PropertyA:asc")]
     public string? OrderBy { get; set; }
-    
-    [SwaggerParameter("A list of properties to include with the query")]
-    public IEnumerable<string>? Includes { get; set; }
 }

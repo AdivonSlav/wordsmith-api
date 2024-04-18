d8f956e961455dd29cf597d451593ae2bfa1f560
AdivonSlav <efendic100@gmail.com>
2024-04-18 22:46:39 +0200

    update docs

--- Wordsmith.Integration/src/GoogleTranslate/IGoogleTranslateService.cs
+++ Wordsmith.Integration/src/GoogleTranslate/IGoogleTranslateService.cs
@@ -5,6 +5,16 @@ namespace Wordsmith.Integration.GoogleTranslate;
 
 public interface IGoogleTranslateService
 {
+    /// <summary>
+    /// Translates the given text 
+    /// </summary>
+    /// <param name="request">Translation request which contains the text, source and target languages</param>
+    /// <returns>The translation response</returns>
     Task<QueryResult<TranslationResponse>> Translate(TranslationRequest request);
+    
+    /// <summary>
+    /// Gets the supported languages for translation
+    /// </summary>
+    /// <returns>The supported languages response</returns>
     Task<QueryResult<SupportedLanguages>> GetSupportedLanguages();
 }
--- Wordsmith.Integration/src/MerriamWebster/IMerriamWebsterService.cs
+++ Wordsmith.Integration/src/MerriamWebster/IMerriamWebsterService.cs
@@ -5,5 +5,10 @@ namespace Wordsmith.Integration.MerriamWebster;
 
 public interface IMerriamWebsterService
 {
+    /// <summary>
+    /// Gets dictionary entries for the given search term
+    /// </summary>
+    /// <param name="searchTerm">The term to search for</param>
+    /// <returns>The dictionary response</returns>
     Task<QueryResult<DictionaryResponse>> GetResults(string searchTerm);
 }
